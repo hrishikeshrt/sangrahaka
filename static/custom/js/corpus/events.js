07e3fc81a07b13f7b7de10456bfd43b30a216814
Hrishikesh Terdalkar <hrishirt@cse.iitk.ac.in>
2022-04-15 16:20:34 +0530

    feat: add javascript for front-end functionality

--- static/custom/js/corpus/events.js
+++ static/custom/js/corpus/events.js
@@ -16,6 +16,7 @@ $(document).ready(function () {
     $entity_root.addClass('custom-auto-complete');
     $relation_source.addClass('custom-auto-complete');
     $relation_target.addClass('custom-auto-complete');
+    $action_actor.addClass('custom-auto-complete');
     $('.custom-auto-complete').autoComplete({
         events: {
             searchPost: function (server_results) {
@@ -34,10 +35,12 @@ $corpus_table.on('check.bs.table', function (e, row, $element, field) {
 $corpus_table.on('expand-row.bs.table', function (e, index, row, $detail) {
     $line_id_entity.val(row.line_id);
     $line_id_relation.val(row.line_id);
+    $line_id_action.val(row.line_id);
     $entity_root.val("");
     $relation_source.val("");
     $relation_target.val("");
     $relation_detail.val("");
+    $action_actor.val("");
 
     var row_roots = new Set();
     $.each(row.analysis, function(index, word) {
@@ -59,6 +62,7 @@ $corpus_table.on('expand-row.bs.table', function (e, index, row, $detail) {
 
     var entity_list_html = [];
     var relation_list_html = [];
+    var action_list_html = [];
 
     $.each(row.entity, function (index, entity) {
         if (!entity.is_deleted) {
@@ -74,8 +78,16 @@ $corpus_table.on('expand-row.bs.table', function (e, index, row, $detail) {
         }
     });
 
+    $.each(row.action, function (index, action) {
+        if (!action.is_deleted) {
+            action_html = action_formatter(action.label, action.actor_label, action.actor, "list-group-item-warning", action.annotator);
+            action_list_html.push(action_html);
+        }
+    });
+
     var unconfirmed = storage.getItem(row.line_id);
     var unconfirmed_relations = storage.getItem(row.line_id + '_relations');
+    var unconfirmed_actions = storage.getItem(row.line_id + '_actions');
 
     if (unconfirmed !== null) {
         $.each(JSON.parse(unconfirmed), function (index, entity) {
@@ -89,15 +101,26 @@ $corpus_table.on('expand-row.bs.table', function (e, index, row, $detail) {
             relation_list_html.push(relation_html);
         });
     }
+    if (unconfirmed_actions !== null) {
+        $.each(JSON.parse(unconfirmed_actions), function (index, action) {
+            action_html = action_formatter(action.label, action.actor_label, action.actor, "list-group-item-warning", action.annotator);
+            action_list_html.push(action_html);
+        });
+    }
+
     $entity_list.html("").append(entity_list_html.join(""));
     $relation_list.html("").append(relation_list_html.join(""));
+    $action_list.html("").append(action_list_html.join(""));
+
     $('[name="entity"]').bootstrapToggle();
     $('[name="relation"]').bootstrapToggle();
+    $('[name="action"]').bootstrapToggle();
 });
 
 $corpus_table.on('page-change.bs.table', function (e, number, size) {
     $line_id_entity.val("");
     $line_id_relation.val("");
+    $line_id_action.val("");
 
     $entity_root.val("");
     $entity_type.val("");
@@ -110,4 +133,9 @@ $corpus_table.on('page-change.bs.table', function (e, number, size) {
     $relation_label.selectpicker('refresh');
     $relation_detail.val("");
     $relation_list.html("")
+
+    $action_label.selectpicker('refresh');
+    $action_actor_label.selectpicker('refresh');
+    $action_actor.val("");
+    $action_list.html("");
 });
--- static/custom/js/corpus/functions.js
+++ static/custom/js/corpus/functions.js
@@ -138,6 +138,34 @@ function relation_formatter(source, label, target, detail, li_classes = "", anno
     return relation_html.join("");
 }
 
+function action_formatter(label, actor_label, actor, li_classes = "", annotator = "") {
+    var action_value = [label, actor_label, actor].join('$');
+    var li_class = 'list-group-item';
+    if (li_classes !== "") {
+        li_class += " " + li_classes;
+    }
+    var action_html = [
+        annotator ? `<li title="${annotator}" class="${li_class}">` : `<li class="${li_class}">`,
+        '<div class="row">',
+        '<div class="col-sm">',
+        `(${label})`,
+        ` <span class="text-muted">⊢ [${actor_label}] →</span> `,
+        `(${actor})`,
+        '</div>',
+        '<div class="col-sm-3">',
+        '<span class="float-right">',
+        `<input type="checkbox" name="action" value="${action_value}" class="mr-5"`,
+        ' data-toggle="toggle" data-size="sm" data-on="<i class=\'fa fa-check\'></i>" ',
+        ' data-off="<i class=\'fa fa-times\'></i>" data-onstyle="success"',
+        ' data-offstyle="danger" checked>',
+        '</span>',
+        '</div>',
+        '</div>',
+        '</li>'
+    ];
+    return action_html.join("");
+}
+
 function row_attribute_handler(row, index) {
     return {}
 }
