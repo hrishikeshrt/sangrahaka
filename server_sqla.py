27b1d958e3681e46c08ef85338565172b92a4328
Hrishikesh Terdalkar <hrishirt@cse.iitk.ac.in>
2023-04-21 10:35:32 +0530

    build(release): bump version 3.2.0 → 3.2.1

--- server_sqla.py
+++ server_sqla.py
@@ -28,7 +28,7 @@ $ python server.py
 
 __author__ = "Hrishikesh Terdalkar"
 __copyright__ = "Copyright (C) 2020-2023 Hrishikesh Terdalkar"
-__version__ = "3.2.0"
+__version__ = "3.2.1"
 
 ###############################################################################
 
